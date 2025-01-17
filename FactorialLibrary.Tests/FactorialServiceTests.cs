8e3e112b2de7cedfdf7c0c93a85896245e2682fb
Halef Caetano <106297533+halefcaetano@users.noreply.github.com>
2025-01-17 23:19:56 +0000

    Add negative factorial feature

--- FactorialLibrary.Tests/FactorialServiceTests.cs
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -1,11 +1,17 @@
-namespace FactorialService.Tests;
-using FactorialLibrary;
-public class FactorialServiceTests
+using Xunit;
+using FactorialLibrary; 
+
+namespace FactorialLibrary.Tests
 {
-    [Fact]
-    public void Test1()
+    public class FactorialServiceTests
     {
-        var result = FactorialService.Factorial(0);
-        Assert.Equal(1, result);
+        [Fact]
+        public void Test_negative()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var result = FactorialService.Factorial(-1);
+            });
+        }
     }
 }
